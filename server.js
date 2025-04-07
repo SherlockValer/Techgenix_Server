e848fa05a29c965b9c971977834f3b7382219f4c
Vaibhav Chopde <vchopde77@gmail.com>
2025-04-07 13:19:58 +0530

    modified:   server.js

--- server.js
+++ server.js
@@ -21,6 +21,8 @@ app.use(express.json())
 const cors = require('cors')
 const corsOptions = {
     origin: '*',
+    credentials: true,
+    optionSuccessStatus: 200,
 }
 app.use(cors(corsOptions))
 
